8a4d0a434fcad23c8b3687ccbbe947a416863e87
Dave Beerens <d.beerens@wappstars.nl>
2022-01-27 15:48:57 +0100

    Assembler for ProductDto and CategoryDto to support HATEOAS

---
+++ src/main/java/com/wappstars/wappfood/assembler/CategoryDtoAssembler.java
@@ -0,0 +1,40 @@
+package com.wappstars.wappfood.assembler;
+
+import com.wappstars.wappfood.controller.CategoryController;
+import com.wappstars.wappfood.dto.CategoryDto;
+import com.wappstars.wappfood.model.Category;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class CategoryDtoAssembler implements RepresentationModelAssembler<Category, CategoryDto> {
+    @Override
+    public CategoryDto toModel(Category entity) {
+
+        CategoryDto categoryDto = CategoryDto.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .slug(entity.getSlug())
+                .description(entity.getDescription())
+                .dateCreated(entity.getDateCreated())
+                .dateModified(entity.getDateModified())
+                .build();
+
+        categoryDto.add(linkTo(methodOn(CategoryController.class).getCategory(categoryDto.getId())).withSelfRel());
+
+        return categoryDto;
+    }
+
+    @Override
+    public CollectionModel<CategoryDto> toCollectionModel(Iterable<? extends Category> entities) {
+        CollectionModel<CategoryDto> categoryDto = RepresentationModelAssembler.super.toCollectionModel(entities);
+
+        categoryDto.add(linkTo(methodOn(CategoryController.class).getCategories()).withSelfRel());
+
+        return categoryDto;
+    }
+}
---
+++ src/main/java/com/wappstars/wappfood/assembler/ProductDtoAssembler.java
@@ -0,0 +1,53 @@
+package com.wappstars.wappfood.assembler;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import com.wappstars.wappfood.controller.ProductController;
+import com.wappstars.wappfood.dto.ProductDto;
+import com.wappstars.wappfood.model.Product;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import java.util.TreeMap;
+
+@Component
+public class ProductDtoAssembler implements RepresentationModelAssembler<Product, ProductDto> {
+    @Override
+    public ProductDto toModel(Product entity) {
+
+        TreeMap<String, String> category = new TreeMap<>();
+        category.put("id", entity.getCategory().getId().toString());
+        category.put("name", entity.getCategory().getName());
+        category.put("slug", entity.getCategory().getSlug());
+
+        ProductDto productDto = ProductDto.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .slug(entity.getSlug())
+                .description(entity.getDescription())
+                .dateCreated(entity.getDateCreated())
+                .dateModified(entity.getDateModified())
+                .sku(entity.getSku())
+                .price(entity.getPrice())
+                .totalSales(entity.getTotalSales())
+                .stockQty(entity.getStockQty())
+                .taxable(entity.isTaxable())
+                .taxClass(entity.getTaxClass())
+                .category(category)
+                .build();
+
+        productDto.add(linkTo(methodOn(ProductController.class).getProduct(productDto.getId())).withSelfRel());
+
+        return productDto;
+    }
+
+    @Override
+    public CollectionModel<ProductDto> toCollectionModel(Iterable<? extends Product> entities) {
+        CollectionModel<ProductDto> productDto = RepresentationModelAssembler.super.toCollectionModel(entities);
+
+        productDto.add(linkTo(methodOn(ProductController.class).getProducts()).withSelfRel());
+
+        return productDto;
+    }
+}
